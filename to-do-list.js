713a3bcc00026ed77e3bb4f56df524dc6d00cef2
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-06-03 14:55:20 -0400

    commit

--- to-do-list.js
+++ to-do-list.js
@@ -1,5 +1,17 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+renderTodoList();
+function renderTodoList(){
+    let todoListHTML = '';
+    for(let i = 0; i < todoList.length; i++){
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
 
+    }
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
@@ -8,4 +20,6 @@ function addTodo(){
     console.log(todoList);
 
     inputElement.value = '';
+
+    renderTodoList();
 }
