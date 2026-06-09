e677c9aef1251120a3e86a47c88c0f1f87afca4c
Sehr Chowdhury <sehr.chowdhury@gmail.com>
2026-06-09 13:45:21 -0400

    commit

--- to-do-list.js
+++ to-do-list.js
@@ -9,10 +9,7 @@ const todoList = [{
 renderTodoList();
 function renderTodoList(){
     let todoListHTML = '';
-    for(let i = 0; i < todoList.length; i++){
-        const todoObject = todoList[i];
-        //const name = todoObject.name;
-        //const dueDate = todoObject.dueDate;
+    todoList.forEach(function(todoObject, index) {
         const {name} = todoObject;
         const {dueDate} = todoObject;
 
@@ -20,13 +17,12 @@ function renderTodoList(){
             <div>${name}</div>
             <div>${dueDate}</div>
                 <button onclick="
-                    todoList.splice(${i}, 1);
+                    todoList.splice(${index}, 1);
                     renderTodoList();
                 " class="delete-button">Delete</button> 
             `;
         todoListHTML += html;
-
-    }
+    });
 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
